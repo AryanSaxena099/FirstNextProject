7aebb9a361379cd5e326f2bfe72e699d303605f3
AryanSaxena099 <saxena.aryan.099@gmail.com>
2025-01-23 20:14:52 +0530

    changes done

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-row justify-center items-center h-lvh w-lvw  font-bold text-3xl space-x-3 bg-red-200"> 
-      this is aryan
+      this is Aryan!!
     </div>
   );
 }
